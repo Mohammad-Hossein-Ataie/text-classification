ef7777d25921544c44fbc683dfdbee1a0b977552
M.H.Ataie <mh.ataie@ut.ac.ir>
2023-01-19 18:35:42 +0330

    Add document size ditector to the python code.

--- textClassification.py
+++ textClassification.py
@@ -2,4 +2,23 @@
 First we add a dataset of text including persian mail, book, proposal, article, proceedings and dissertation.
 """
 from docx import Document
-document = Document("test.docx")
+import pandas as pd
+"""
+Then we should add a function to find number of words in each document.
+"""
+
+
+def setDocSize(dataFrame):
+    documentSize = 0
+    for i in range(len(dataFrame[0])):
+        documentSize += len(dataFrame[0][i].split(" "))
+    return documentSize
+
+
+if __name__ == "__main__":
+    document = Document("test.docx")
+    paras = [p.text for p in document.paragraphs if p.text]
+    df = pd.DataFrame(paras)
+    docSize = setDocSize(df)
+
+
