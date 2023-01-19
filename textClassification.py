5daf80b28a923417f6202539d9da7d31920cb848
M.H.Ataie <mh.ataie@ut.ac.ir>
2023-01-19 18:40:16 +0330

    Add a function to check where the given string occured in the document.

--- textClassification.py
+++ textClassification.py
@@ -3,16 +3,26 @@ First we add a dataset of text including persian mail, book, proposal, article, 
 """
 from docx import Document
 import pandas as pd
-"""
-Then we should add a function to find number of words in each document.
-"""
 
 
 def setDocSize(dataFrame):
+    """
+    Then we should add a function to find number of words in each document.
+    """
     documentSize = 0
     for i in range(len(dataFrame[0])):
         documentSize += len(dataFrame[0][i].split(" "))
     return documentSize
+
+
+def firstOccurrence(string):
+    """
+    Add a firstOccurrence to check where a string occurs in the document for the first time.
+    """
+    for i in range(len(df[0].str.count(string))):
+        if df[0].str.count(string)[i] == 1:
+            return i
+    return -1
 
 
 if __name__ == "__main__":
