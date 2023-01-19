6068157378d5d1733af0dfb94d3867c4f44a58c8
M.H.Ataie <mh.ataie@ut.ac.ir>
2023-01-19 18:44:36 +0330

    Add a function to determine the occurrence of the list of words.

--- textClassification.py
+++ textClassification.py
@@ -25,6 +25,28 @@ def firstOccurrence(string):
     return -1
 
 
+def checkOccurence(words):
+    """
+    A function to determine the occurrence of the list of words. Actually finds a list of strings and informs us of each
+    occurrence in a final list.)
+    """
+    rlist = []
+    for j in range(len(words)):
+        flag = 0
+        if len(words[j]) != 2:
+            rlist.append(firstOccurrence(words[j]))
+        else:
+            for x in range(len(words[j])):
+                if firstOccurrence(words[j][x]) == -1:
+                    continue
+                else:
+                    flag = 1
+                    rlist.append(firstOccurrence(words[j][x]))
+            if flag == 0:
+                rlist.append(-1)
+    return rlist
+
+
 if __name__ == "__main__":
     document = Document("test.docx")
     paras = [p.text for p in document.paragraphs if p.text]
